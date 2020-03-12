a88af6e3d0d87a53cd0162bb6e72d80be78140be
mykw ysi <miyakawaysi@outlook.jp>
2020-03-12 17:02:16 +0900

    とりあえず時間割ページhtml貼り付けた

--- src/App.js
+++ src/App.js
@@ -1,4 +1,69 @@
 import React from "react"
 
 function App() {
+  <body>
+    <header>
+      <h1>鬼仏表+</h1>
+      <div class="register-icon"></div>
+    </header>
+
+    <section class="jikanwari">
+
+      <div class="j-items">
+
+        <ul class="one">
+          <li>理論社会学<input type="checkbox"></li>
+            <li><input type="checkbox"></li>
+              <li><input type="checkbox"></li>
+                <li><input type="checkbox"></li>
+                  <li><input type="checkbox"></li>
+        </ul>
+
+                  <ul class="two">
+                    <li>環境法<input type="checkbox"></li>
+                      <li><input type="checkbox"></li>
+                        <li><input type="checkbox"></li>
+                          <li><input type="checkbox"></li>
+                            <li><input type="checkbox"></li>
+        </ul>
+
+                            <ul class="three">
+                              <li><input type="checkbox"></li>
+                                <li><input type="checkbox"></li>
+                                  <li>マイノリティ論<input type="checkbox"></li>
+                                    <li><input type="checkbox"></li>
+                                      <li><input type="checkbox"></li>
+        </ul>
+
+                                      <ul class="four">
+                                        <li><input type="checkbox"></li>
+                                          <li><input type="checkbox"></li>
+                                            <li><input type="checkbox"></li>
+                                              <li><input type="checkbox"></li>
+                                                <li><input type="checkbox"></li>
+        </ul>
+
+                                                <ul class="five">
+                                                  <li><input type="checkbox"></li>
+                                                    <li><input type="checkbox"></li>
+                                                      <li><input type="checkbox"></li>
+                                                        <li><input type="checkbox"></li>
+                                                          <li><input type="checkbox"></li>
+        </ul>
+        
+      </div>
+
+                                                        <div class="btns">
+                                                          <button type="button">全消去</button>
+                                                          <button type="button">選択済み消去</button>
+                                                        </div>
+
+                                                        <form action="#">
+                                                          <p>memo:</p>
+                                                          <textarea cols="30" rows="10"></textarea>
+                                                          <button type="submit">追加</button>
+                                                        </form>
+
+    </section>
+  </body>
 }
