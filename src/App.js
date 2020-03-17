913863d431bc7a181c7610d222a4a9647c08e272
Tsuyoshi-I <tsuyoshifl244@gmail.com>
2020-03-17 12:07:45 +0900

    App.js html整形

--- src/App.js
+++ src/App.js
@@ -1,69 +1,66 @@
 import React from "react"
 
-function App() {
-  <body>
-    <header>
-      <h1>鬼仏表+</h1>
-      <div class="register-icon"></div>
-    </header>
+const App = () => {
+  return (
+    <>
+      <header>
+        <h1>鬼仏表+</h1>
+        <div className="register-icon"></div>
+      </header>
+      <section className="jikanwari">
+        <div className="j-items">
+          <ul className="one">
+            <li>理論社会学<input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+          </ul>
+          <ul className="two">
+            <li>環境法<input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+          </ul>
+          <ul className="three">
+            <li><input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+            <li>マイノリティ論<input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+          </ul>
+          <ul className="four">
+            <li><input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+          </ul>
+          <ul className="five">
+            <li><input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+            <li><input type="checkbox" /></li>
+          </ul>
 
-    <section class="jikanwari">
+        </div>
 
-      <div class="j-items">
+        <div className="btns">
+          <button type="button">全消去</button>
+          <button type="button">選択済み消去</button>
+        </div>
 
-        <ul class="one">
-          <li>理論社会学<input type="checkbox"></li>
-            <li><input type="checkbox"></li>
-              <li><input type="checkbox"></li>
-                <li><input type="checkbox"></li>
-                  <li><input type="checkbox"></li>
-        </ul>
+        <form action="#">
+          <p>memo:</p>
+          <textarea cols="30" rows="10"></textarea>
+          <button type="submit">追加</button>
+        </form>
 
-                  <ul class="two">
-                    <li>環境法<input type="checkbox"></li>
-                      <li><input type="checkbox"></li>
-                        <li><input type="checkbox"></li>
-                          <li><input type="checkbox"></li>
-                            <li><input type="checkbox"></li>
-        </ul>
-
-                            <ul class="three">
-                              <li><input type="checkbox"></li>
-                                <li><input type="checkbox"></li>
-                                  <li>マイノリティ論<input type="checkbox"></li>
-                                    <li><input type="checkbox"></li>
-                                      <li><input type="checkbox"></li>
-        </ul>
-
-                                      <ul class="four">
-                                        <li><input type="checkbox"></li>
-                                          <li><input type="checkbox"></li>
-                                            <li><input type="checkbox"></li>
-                                              <li><input type="checkbox"></li>
-                                                <li><input type="checkbox"></li>
-        </ul>
-
-                                                <ul class="five">
-                                                  <li><input type="checkbox"></li>
-                                                    <li><input type="checkbox"></li>
-                                                      <li><input type="checkbox"></li>
-                                                        <li><input type="checkbox"></li>
-                                                          <li><input type="checkbox"></li>
-        </ul>
-        
-      </div>
-
-                                                        <div class="btns">
-                                                          <button type="button">全消去</button>
-                                                          <button type="button">選択済み消去</button>
-                                                        </div>
-
-                                                        <form action="#">
-                                                          <p>memo:</p>
-                                                          <textarea cols="30" rows="10"></textarea>
-                                                          <button type="submit">追加</button>
-                                                        </form>
-
-    </section>
-  </body>
+      </section>
+    </>
+  )
 }
+
+export default App
